4e95aaec9ea68389d64577f8b3ce4d08f2e1eda8
Pablo Araya <pablo.araya@kinnser.com>
2017-09-04 11:44:02 -0300

    Add endpoints beta

---
+++ MorfAr.Web/Models/Domain/Location.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MorfAr.Web.Models.Domain
+{
+    public class Location
+    {
+        public int locationId {get; set; }
+        public string locationName { get; set; }
+    }
+}
