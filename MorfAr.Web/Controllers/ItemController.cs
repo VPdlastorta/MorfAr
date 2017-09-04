77b76e933dc03bc7123aa52b1f4605ecd7480623
Pablo Araya <pablo.araya@kinnser.com>
2017-09-04 10:05:28 -0300

    Add default controllers

---
+++ MorfAr.Web/Controllers/ItemController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MorfAr.Web.Controllers
+{
+    public class ItemController : ApiController
+    {
+        // GET: api/Item
+        [Route("api/Item")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Item/5
+        [Route("api/Item/{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Item
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Item/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Item/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
