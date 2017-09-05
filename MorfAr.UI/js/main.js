b0c041362fa3daf7e7456504e38b9d42070f0800
manubertelli <manubertelli@hotmail.com>
2017-09-05 14:33:34 -0300

    carga de resultados dinamica menos direccion

--- MorfAr.UI/js/main.js
+++ MorfAr.UI/js/main.js
@@ -38,14 +38,40 @@ function doSearch(){
     var searchItem = $('#foodFilter').val();
     var searchLocation = $('#citiesList > option').attr( "locationId" );
     var searchUrl = "http://vpros-dlastorta/MorfAr/api/Item?search="+searchItem+"&locationId="+searchLocation;
-    console.log(searchUrl);
+    // console.log(searchUrl);
     var results = $.getJSON(searchUrl, function (data) {
         var items = [];
         $(".container.accordion").empty();
         $.each(data, function (i) {
             var currentResult = results.responseJSON[i];
-            console.log(currentResult);
-            $(".container.accordion").append('<div class="accordion-section"><a class="accordion-section-title" href="#accordion-'+i+'">'+currentResult.itemName+'</a><div id="accordion-'+i+'" class="accordion-section-content open"><div class="row"> <div class="col-md-6">    <p class="place">'+currentResult.place.placeName+'</p><p class="adress">Pellegrini: 1234</p><p class="delivery">¿Tiene delivery?: Si</p><p class="delivery">¿Tiene para llevar?: Si</p><p class="score">8 puntos</p><img class="food-img" src="'+currentResult.itemUrlPhoto+'"></img></div><div class="col-md-6"><p class="score">Comentarios</p>  <p class="comments">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sodales mollis quam ac tincidunt. Vivamus tincidunt posuere tellus, sed laoreet orci auctor e"</p></div></div>');
+            var delivery = currentResult.place.placeIsDelivery;
+            var takeAway = currentResult.place.placeIsTakeAway;
+            var haveDelivery;
+            var haveTakeAway;
+
+            if( delivery ){
+                haveDelivery = "Si";
+            }else{
+                haveDelivery = "No";
+            }
+            console.log('delivery es '+delivery+' y haveDelivery es '+ haveDelivery );
+            
+            if( takeAway ){
+                haveTakeAway = "Si";
+            }else{
+                haveTakeAway = "No";
+            }
+            // console.log(currentResult);
+            $(".container.accordion").append('<div class="accordion-section"><a class="accordion-section-title" href="#accordion-'+i+'">'+currentResult.itemName+'</a> <div id="accordion-'+i+'" class="accordion-section-content open"> <div class="row"> <div class="col-md-6"> <p class="place">'+currentResult.place.placeName+'</p> <p class="adress">Pellegrini: 1234</p> <p class="delivery">¿Tiene delivery?: '+haveDelivery+'</p> <p class="delivery">¿Tiene para llevar?: '+haveTakeAway+'</p> <p class="score">8 puntos</p> </div> <div class="col-md-6"><img class="food-img" src="'+currentResult.itemUrlPhoto+'"></img> </div> </div> <div class="row"> <div class="col-md-6"> <div class="col-md-12">  <div>'+currentResult.place.googleMapsUrl+'</div> </div> </div> <div class="col-md-6"> <p class="score">Comentarios</p> <ul id="comments"></ul> </div> </div>');
+            
+            var commentsList = currentResult.itemReviews;
+            // console.log(commentsList);
+            var comments = [];
+            for( k=0; k < commentsList.length; k++){
+                var currentComment = commentsList[k].comment;
+                console.log('comentario nuemro '+k+currentComment);
+                $("#comments").append('<li class="comment">'+currentComment+'</li>');
+            };
         });
         accordion();
     });
