887a0a411fa4bc9102d55957e7cec00e11ad2785
Gabriel Giri <ggiri@Gabriels-MacBook-Pro.local>
2017-09-12 17:39:12 -0300

    new styling feature

--- MorfAr.UI/js/main.js
+++ MorfAr.UI/js/main.js
@@ -49,6 +49,18 @@ function doSearch(){
             var haveDelivery;
             var haveTakeAway;
 
+        if (currentResult.scoreAvg > 7) {
+            var scoreColor ="00FF48";
+
+        } else if (currentResult.scoreAvg > 4){
+            var scoreColor ="ff8800";
+
+        } else {
+            var scoreColor ="B20000";
+
+        }
+
+                  
             if( delivery ){
                 haveDelivery = "Si";
             }else{
@@ -61,17 +73,18 @@ function doSearch(){
             }else{
                 haveTakeAway = "No";
             }
-            
 
-console.log(currentResult);
-            $(".container.accordion").append('<div class="accordion-section"><a class="accordion-section-title" href="#accordion-'+i+'" style="border-right: solid #ff8800 '+(currentResult.scoreAvg*10)+'px">'+currentResult.itemName+"  |  "+currentResult.place.placeName+'<span>'+currentResult.scoreAvg+' puntos</span></a> <div id="accordion-'+i+'" class="accordion-section-content open"> <div class="row"> <div class="col-md-6"> <p class="place">'+currentResult.place.placeName+'</p> <p class="adress">Pellegrini: 1234</p> <p class="delivery">¿Tiene delivery?: '+haveDelivery+'</p> <p class="delivery">¿Tiene para llevar?: '+haveTakeAway+'</p> <p class="score">'+currentResult.scoreAvg+' puntos</p> </div> <div class="col-md-6"><img class="food-img" src="'+currentResult.itemUrlPhoto+'"></img> </div> </div> <div class="row"> <div class="col-md-6"> <div class="col-md-12">  <div>'+currentResult.place.googleMapsUrl+'</div> </div> </div> <div class="col-md-6"> <p class="score">Comentarios</p> <ul id="comments"></ul> </div> </div>');
+var accordionTab =' <a class="accordion-section-title" href="#accordion-'+i+'" style="border-right: solid #'+scoreColor+' '+(currentResult.scoreAvg*10)+'px">'+currentResult.itemName+"  |  "+currentResult.place.placeName+'<span>'+currentResult.scoreAvg+' puntos</span></a>';
+var leftCol ='<div class="col-md-6"> <p class="place">'+currentResult.place.placeName+'</p> <p class="delivery">¿Tiene delivery?: '+haveDelivery+'</p> <p class="delivery">¿Tiene para llevar?: '+haveTakeAway+'</p> <img class="food-img" src="'+currentResult.itemUrlPhoto+'"></img></div>';
+var rightCol ='<div class="col-md-6"> <p class="score">Comentarios</p> <ul id="comments"></ul> </div>';
+var googleMap ='<div class="resultMap">'+currentResult.place.googleMapsUrl+'</div>';
+console.log (currentResult.place.googleMapsUrl);
+            $(".container.accordion").append('<div class="accordion-section">' + accordionTab + ' <div id="accordion-'+i+'" class="accordion-section-content open"> <div class="row">' + leftCol + rightCol + '  </div> <div class="row"> <div class="col-md-6"> <div class="col-md-12">  ' + googleMap + ' </div> </div> </div>');
             
             var commentsList = currentResult.itemReviews;
-            // console.log(commentsList);
             var comments = [];
             for( k=0; k < commentsList.length; k++){
                 var currentComment = commentsList[k].comment;
-                //console.log('comentario nuemro '+k+currentComment);
                 $("#comments").append('<li class="comment">'+currentComment+'</li>');
             };
         });
